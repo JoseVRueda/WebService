5e38d578878d896ffebca35b57d1a3ea0fdbd39b
Yuli Andrea Meza Barros <Vjrueda@uninorte.edu.co>
2024-04-08 11:21:24 -0500

    Completed

--- lib/domain/repositories/user_repository.dart
+++ lib/domain/repositories/user_repository.dart
@@ -6,20 +6,27 @@ import '../../data/models/random_user_model.dart';
 import '../entities/random_user.dart';
 
 class UserRepository {
-
+  late UserRemoteDataSource _userRemoteDataSource;
+  late UserLocalDataSource _userLocalDataSource;
 
   UserRepository() {
     logInfo("Starting UserRepository");
   }
 
   Future<bool> getUser() async {
+    RandomUser user = await _userRemoteDataSource.getUser();
+    _userLocalDataSource.addUser(user);
+    return true;
   }
 
   Future<List<RandomUser>> getAllUsers() async =>
+      await _userLocalDataSource.getAllUsers();
 
   Future<void> deleteUser(id) async =>
+      await _userLocalDataSource.deleteUser(id);
 
-  Future<void> deleteAll() async => 
+  Future<void> deleteAll() async => await _userLocalDataSource.deleteAll();
 
-  Future<void> updateUser(user) async => 
+  Future<void> updateUser(user) async =>
+      await _userLocalDataSource.updateUser(user);
 }
--- lib/domain/use_case/users.dart
+++ lib/domain/use_case/users.dart
@@ -5,10 +5,11 @@ import '../entities/random_user.dart';
 import '../repositories/user_repository.dart';
 
 class Users {
+  final UserRepository _repository = Get.find();
 
-  Future<void> addUser() async => 
-  Future<List<RandomUser>> getAllUsers() async =>
-  Future<void> deleteUser(id) async => 
-  Future<void> deleteAll() async =>  
-  Future<void> updateUser(user) async => 
+  Future<void> addUser() async => await _repository.getUser();
+  Future<List<RandomUser>> getAllUsers() async => await _repository.getAllUsers();
+  Future<void> deleteUser(id) async => await _repository.deleteUser(id);
+  Future<void> deleteAll() async => await _repository.deleteAll();
+  Future<void> updateUser(user) async => await _repository.updateUser(user);  
 }
